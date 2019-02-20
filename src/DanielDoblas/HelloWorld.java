9643b7b271f844aef8b1c0e12081d0c0aad2e58a
Daniel Doblas Jimenez <dandobjim@uma.es>
2019-02-20 11:34:57 +0100

    Adicion de el paquete Daniel Doblas y de los archivos del dia 1 para la realizacion de los ejercicios en la rama DanielDoblas en el repositorio del profesor en github

---
+++ src/DanielDoblas/HelloWorld.java
@@ -0,0 +1,18 @@
+package DanielDoblas;
+
+import javax.swing.SwingUtilities;
+
+public class HelloWorld {
+	public static void main(String [ ] args) {
+		//Create and set up the window.
+		SwingUtilities . invokeLater(new Runnable ( ) {
+			public void run ( ) {
+				MyOwnFrame frame = new MyOwnFrame("FrameDemo2") ;
+				//Display the window.
+				frame . pack ( ) ;
+				frame . setVisible(true) ;
+			}
+		} ) ;
+	}
+}
+
