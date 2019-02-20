3c66a7fd6734a4b7b6796885a28daaabfbb9f48b
Daniel Doblas Jimenez <dandobjim@uma.es>
2019-02-20 16:17:18 +0100

    Eliminacion de InterfazFrame.java y creacion de una aplicación con dos paneles, uno arriba con una etiqueta y un área de texto en el que el usuario pueda escribir y

--- src/DanielDoblas/MyOwnFrame.java
+++ src/DanielDoblas/MyOwnFrame.java
@@ -1,50 +1,64 @@
 package DanielDoblas;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
+import java.awt.Container;
+import java.awt.GridLayout;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
+
+//Crear una aplicación con dos paneles, uno arriba con una etiqueta y un área de texto en el que el usuario pueda escribir y
+//otro panel abajo con un botón con la fecha y hora actuales y un desplegable con tres opciones distintas.
+
+
+
+
 
 public class MyOwnFrame extends JFrame{
+	String strdate=null;
+	JComboBox combo;
+	JButton button1 = new JButton(strdate);
+	GridLayout Layout = new GridLayout(0,2);
+	
+	Calendar calendardate = Calendar.getInstance();
 
-	public MyOwnFrame(String s) {
-		super (s);
-		//Que hacer al cerrar
-		setDefaultCloseOperation(JFrame . EXIT_ON_CLOSE) ;
-		
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy 'a las' HH:mm:ss");
 	
-		
-		//Añadir Label B
-		JLabel bLabel = new JLabel("Fecha Actual") ;
-		bLabel . setPreferredSize(new Dimension( 100 , 100) ) ;
-		// Añadimos Label B al layout en el centro usando BorderLayout
-		getContentPane ( ) . add(bLabel,BorderLayout.CENTER) ;
-		
-		
-		// Añadir Calendario
-		
-		Calendar calendardate = Calendar.getInstance();
-		
-		JLabel clabel = new JLabel(calendardate.getTime().toString());
-		getContentPane ( ) . add(clabel,BorderLayout.EAST) ;
 	
-		String strdate=null;
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy 'a las' HH:mm:ss") ;
-		if(calendardate!= null){
-			strdate=sdf.format(calendardate.getTime( ) ) ;
-		}
-		clabel.setText(strdate);
-		
+	public MyOwnFrame (String s) {
+		super(s);
 	
-		//Creamos el boton
-		JButton boton1 = new JButton(strdate);
-		//Añadimos el boton debajo de la fecha
-		getContentPane().add(boton1, BorderLayout.SOUTH);
-		//Ajusta todos los componentes del frame al tamaño maximo
-		this.pack();
+
+	}
+	
+	public void getFecha() {
+		if (calendardate!=null) {
+			strdate = sdf.format(calendardate.getTime());
+		};
+
+	}
+	
+	public void addComponentsToPane(final Container pane) {
+		final JPanel arriba = new JPanel();
+		arriba.setLayout(Layout);
+		JPanel abajo = new JPanel();
+		abajo.setLayout(new GridLayout(2,2));
+		getFecha();
+		abajo.add(button1);
+		abajo.add(combo);
+		
+		 pane.add(arriba, BorderLayout.NORTH);
+	     pane.add(new JSeparator(), BorderLayout.CENTER);
+	     pane.add(abajo, BorderLayout.SOUTH);
+		
 	}
 
+	
+	
 }
